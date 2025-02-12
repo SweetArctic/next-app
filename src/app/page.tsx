2713d2f485436a970478dcd5862c705d9b7ece02
Juan Fernando <127075072+SweetArctic@users.noreply.github.com>
2025-02-12 09:52:40 -0500

    #
    Create a card with a landscape

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,22 @@
 import Image from "next/image";
+import Landscape from "./Image.jpg";
 
-export default function Home() {
+export default function Card() {
   return (
-    <main className="grid m-10 grid-cols-4 gap-4 text-white">
-      <span className="bg-pink-600 rounded-lg">1</span>
-      <span className="bg-pink-600 rounded-lg">2</span>
-      <span className="bg-pink-600 rounded-lg">3</span>
-      <span className="bg-pink-600 rounded-lg">4</span>
-      <span className="bg-pink-600 rounded-lg">5</span>
-      <span className="bg-pink-600 rounded-lg">6</span>
-      <span className="bg-pink-600 rounded-lg">7</span>
-      <span className="bg-pink-600 rounded-lg">8</span>
-      <span className="bg-pink-600 rounded-lg">9</span>
-    </main>
+    <section className="grid w-80 rounded-[15px] shadow-md overflow-hidden border-2 border-red-300">
+      <figure className="p-0 flex items-center justify-center bg-gradient-to-r from-red-500 to-orange-500 p-4">
+        <Image src={Landscape} alt="Red Heaven" className=""/>
+      </figure>
+      <article className="grid gap-2 p-4 bg-white">
+        <h2 className="text-lg font-bold text-gray-900">Red Heaven</h2>
+        <p className="text-sm text-gray-600">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt aspernatur expedita eum recusandae, sed voluptatibus laudantium quibusdam ipsa doloremque sequi vitae rerum assumenda commodi libero soluta ex impedit at ullam?
+        </p>
+      </article>
+      <footer className="flex gap-2 p-4 bg-white">
+        <span className="px-3 py-1 text-xs font-semibold text-white bg-red-500 rounded-full"> #tailwind</span>
+        <span className="px-3 py-1 text-xs font-semibold text-white bg-red-500 rounded-full"> #frontendeverything </span>
+      </footer>
+    </section>
   );
 }
